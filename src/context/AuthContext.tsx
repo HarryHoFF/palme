5ee36dec7d46441818c96f41a0aa5e8cdb363f60
HarryHoFF <caseygs1984@gmail.com>
2025-10-12 19:30:28 +0200

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -8,7 +8,7 @@ const mockUser: User = {
   username: 'GoonDocksHaderslev',
   email: 'goondocks@haderslev.dk',
   fullName: 'GoonDocks Haderslev',
-  balance: 6773.92
+  balance: 6819.72
 };
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
