5ffa7ecec965d27a633e965122e9653e062f6787
HarryHoFF <caseygs1984@gmail.com>
2025-10-19 21:05:39 +0200

    Update TransactionContext.tsx

--- src/context/TransactionContext.tsx
+++ src/context/TransactionContext.tsx
@@ -12,6 +12,15 @@ const TransactionContext = createContext<TransactionContextType | undefined>(und
 
 const mockTransactions: Transaction[] = [
   {
+                                              id: '271',
+    type: 'payment',
+    amount: 37.14,
+    recipient: 'Deutsche Post AG',
+    description: 'Shipping fee',
+    date: new Date('2025-10-19'),
+    status: 'completed'
+  },
+  {
                                                           id: '270',
     type: 'received',
     amount: 57.80,
