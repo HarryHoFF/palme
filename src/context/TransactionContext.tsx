f43123fb2a8c3a94ad9f0a13b65727f6d8e4882c
HarryHoFF <caseygs1984@gmail.com>
2026-01-01 23:12:02 +0100

    Update TransactionContext.tsx

--- src/context/TransactionContext.tsx
+++ src/context/TransactionContext.tsx
@@ -12,12 +12,13 @@ const TransactionContext = createContext<TransactionContextType | undefined>(und
 
 const mockTransactions: Transaction[] = [
   {
+    
                                                                                                                                                                                                                                                                                                                                         id: '270',
     type: 'received',
-    amount: 78.85,
-    sender: 'Selina Schmitt',
+    amount: 75.80,
+    sender: 'Mareike Mohr',
     description: 'Payment received',
-    date: new Date('2025-12-30'),
+    date: new Date('2026-01-01'),
     status: 'completed'
   },
   {
